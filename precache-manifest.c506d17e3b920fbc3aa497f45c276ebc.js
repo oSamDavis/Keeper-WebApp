8b5fa208598820eeb0bf5b18a8e32536ebb37aa8
oSamDavis <osamdavis@gmail.com>
2021-06-14 02:53:37 -0500

    Updates

---
+++ precache-manifest.c506d17e3b920fbc3aa497f45c276ebc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152bb6e14c05946f76271423122d14e5",
+    "url": "/Keeper-WebApp/index.html"
+  },
+  {
+    "revision": "38414a6cfd5f699cba4e",
+    "url": "/Keeper-WebApp/static/js/2.89d26e2f.chunk.js"
+  },
+  {
+    "revision": "2e86448a8a2709c15265",
+    "url": "/Keeper-WebApp/static/js/main.168ee3fd.chunk.js"
+  },
+  {
+    "revision": "849f9f852970d8c8f6d0",
+    "url": "/Keeper-WebApp/static/js/runtime-main.e93218c6.js"
+  }
+]);
